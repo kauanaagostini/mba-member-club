d822a4bf133d26b347158ecf56a8cb52cf33fbfc
Kauana Maria Agostini <kauanaagostini@gmail.com>
2024-08-02 21:02:37 -0300

    feat: create first css, and include de css on the package.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+
 
 module.exports= {
   target: "web",
@@ -23,6 +25,14 @@ module.exports= {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "index.html"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"),
+          to: path.resolve(__dirname, "dist", "src", "assets")
+        },
+      ],
     }),
   ],
 
